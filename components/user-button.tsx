cb85aacf90807ad08ecff8349045997ddf890886
unknown <2006mahiro0224@gmail.com>
2024-12-13 18:09:44 +0900

    ある程度authのコード書いたがエラーある

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -6,17 +6,20 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import { SignOut } from "./auth-components";
+import { SignIn, SignOut } from "./auth-components";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { auth } from "@/auth";
 
 export default async function UserButton() {
+  const session = await auth();
+  if (!session?.user) return <SignIn provider="github" />;
   return (
     <div className="flex gap-2 items-center">
       <span className="hidden text-sm sm:inline-flex"></span>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-            <Avatar className="w-8 h-8"></Avatar>
+            <Avatar className="w-8 h-8">avatar</Avatar>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="end" forceMount>
