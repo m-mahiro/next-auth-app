4a92cf2106756377246da2bfec4dd2b68dcf3e27
unknown <2006mahiro0224@gmail.com>
2024-12-15 11:04:42 +0900

    ログイン実装

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -19,14 +19,21 @@ export default async function UserButton() {
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-            <Avatar className="w-8 h-8">avatar</Avatar>
+            <Avatar className="w-8 h-8">
+              {
+                session.user.image && (
+                  <AvatarImage src={session.user.image} alt={session.user.name ?? ''} />
+                )
+              }
+              <AvatarFallback>{session.user.email}</AvatarFallback>
+            </Avatar>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none"></p>
-              <p className="text-xs leading-none text-muted-foreground"></p>
+              <p className="text-sm font-medium leading-none">{session.user.name}</p>
+              <p className="text-xs leading-none text-muted-foreground">{session.user.email}</p>
             </div>
           </DropdownMenuLabel>
           <DropdownMenuItem>
